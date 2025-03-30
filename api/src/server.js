da38f410a6ae1f8f13bdf98abcb804b55cc2d441
nilswag <nilswagemaker05@icloud.com>
2025-03-30 18:26:22 +0200

    removed redundant production/development setup and added .env file

--- api/src/server.js
+++ api/src/server.js
@@ -9,5 +9,5 @@ app.get("/", (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`MiniatURL API listenting at http://localhost:${ port }`);
+  console.log(`MiniatURL API listenting at http://localhost: ${ port }`);
 })
