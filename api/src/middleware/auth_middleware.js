96b3eeff8d7e5787c784d2b5503b61b55075ec67
nilswag <nilswagemaker05@icloud.com>
2025-04-04 15:00:30 +0200

    token is now saved as cookie for persistant storage

--- api/src/middleware/auth_middleware.js
+++ api/src/middleware/auth_middleware.js
@@ -35,6 +35,17 @@ const authenticate = (req, res, next) => {
       process.env.JWT_SECRET,
       { expiresIn: "30d" }
     );
+
+    res.cookie(
+      "auth_token",
+      token,
+      {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: "Lax",
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+      }
+    );
   }
 
   try {
